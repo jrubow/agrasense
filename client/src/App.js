5249dee500941c3081cddb9ce803c29a3f6ba1a5
jrubow <143541334+jrubow@users.noreply.github.com>
2025-02-22 09:53:32 -0500

    Added navigation to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DeployPage from "./pages/DeployPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
-  );
+    <BrowserRouter>
+      <NavBar/>
+      <div className="navbar-margin">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<DeployPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
