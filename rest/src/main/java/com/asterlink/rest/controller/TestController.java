d3acc5e9db1378065000ed584e7b24a66b26a318
Gleb Bereziuk <hbereziu@purdue.edu>
2025-02-23 04:55:54 -0500

    fix

--- rest/src/main/java/com/asterlink/rest/controller/TestController.java
+++ rest/src/main/java/com/asterlink/rest/controller/TestController.java
@@ -4,12 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
+public class TestController {
 
-    @GetMapping("/")
-    public String home()
+    @GetMapping("/") public String health()
     {
-        return "AsterLink: Home";
+        return "AsterLink: TestController";
     }
 }
-
