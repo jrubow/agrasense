be8fe9ee773893c7acd8c48b18927e38958799da
jrubow <143541334+jrubow@users.noreply.github.com>
2025-03-23 00:10:19 -0400

    Added Remote Client

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,39 +1,15 @@
-import React, { useState } from "react";
-import { MdHome, MdRocketLaunch } from "react-icons/md";
-import { Link } from 'react-router-dom';
+import React from 'react';
+import "../css/components/navbar.css";
 
-function NavBar() {
-    const [selected, setSelected] = useState("home");
-
-    return (
-        <div className="navbar-container">
-            <div className="navbar">
-                <div
-                    className={`slider ${selected === "deploy" ? "slide-right" : "slide-left"}`}
-                ></div>
-                
-                <Link to="/">
-                    <div
-                        className={`nav-item ${selected === "home" ? "active" : ""}`}
-                        onClick={() => setSelected("home")}
-                    >
-                        
-                        <MdHome className="react-icon" />
-                        
-                    </div>
-                </Link>
-                <Link to="/deploy">
-                    <div
-                        className={`nav-item ${selected === "deploy" ? "active" : ""}`}
-                        onClick={() => setSelected("deploy")}
-                    >
-                        
-                        <MdRocketLaunch className="react-icon" />
-                    </div>
-                </Link>
-            </div>
-        </div>
-    );
-}
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/map">Map</a></li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
---
+++ client/src/pages/DevicePage.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from 'react';
+import {Link, useNavigate} from "react-router-dom"
+import '../css/pages/devicepage.css';
+import Plot from 'react-plotly.js';
+import axios from "axios"
+
+const DevicePage = () => {
+  // Extract query parameters from URL
+  const queryParams = new URLSearchParams(window.location.search)
+  const sentinel = queryParams.get('sentinel')
+  const deviceId = queryParams.get('device_id')
+  const [relayDevices, setRelayDevices] = useState([])
+  const [device, setDevice] = useState(null)
+  const [tempN, setTempN] = useState([])
+  const navigate = useNavigate()
+
+  const layout = {
+    title: 'Temperature Changes Over Time',
+    xaxis: {
+      title: 'Time',
+      tickangle: -45,  // Rotate the time labels for better readability
+    },
+    yaxis: {
+      title: 'Temperature (°C)',
+    },
+    margin: {
+      t: 20,
+      r: 20,
+      b: 80,
+      l: 20,
+    },
+  };
+
+
+  useEffect(() => {
+    async function getDevice() {
+        if (sentinel === '1') {
+            try {
+                const relayResponse = await axios.get(`/api/devices/relay/network/${deviceId}`)
+                setRelayDevices(relayResponse.data);
+                const deviceResponse = await axios.get(`/api/devices/sentinel/get/${deviceId}`)
+                console.log(deviceResponse.data)
+                setDevice(deviceResponse.data)
+            } catch (error) {
+                console.error("Error getting relay devices:", error)
+            }
+            
+        } else {
+            try {
+                const response = await axios.get(`/api/devices/relay/get/${deviceId}`)
+                setDevice(response.data);
+            } catch (error) {
+                console.error("Error getting relay devices:", error)
+            }
+        }
+    }
+
+    async function getNRecords() {
+        try {
+            const response = await axios.get(`/api/record/recent?device_id=${deviceId}&n=25`)
+            setTempN(response.data)
+            console.log(response.data)
+
+        } catch (error) {
+            console.error("Error getting n records", error)
+        }
+    }
+
+    getDevice()
+    getNRecords()
+  }, [sentinel, deviceId])
+
+  return (
+    <div className="devicepage-top-container">
+        <div className="devicepage-info-container">
+            {device != null ? <div className="devicepage-container">
+                <h1> {sentinel === "1" ? "Sentinel " : "Relay "}Device Details</h1>
+                <div className="device-info">
+                    <p><strong>Device ID:</strong> {device.device_id}</p>
+                    <p><strong>Battery Life:</strong> {device.battery_life}</p>
+                    <p><strong>Is Deployed:</strong> {device.deployed ? "Yes" : "No"}</p>
+                    <p><strong>Deployed Date:</strong> {device.deployed_date}</p>
+                    <p><strong>Is Connected:</strong> {device.is_connected ? "Yes" : "No"}</p>
+                    <p><strong>Last Online:</strong> {device.last_online}</p>
+                    <p><strong>Latitude:</strong> {device.latitude}</p>
+                    <p><strong>Longitude:</strong> {device.longitude}</p>
+                    {sentinel === "1" ? 
+                        <p><strong># of Connected Devices:</strong> {relayDevices.length}</p> 
+                        : 
+                        <p><strong>Sentinel Id:</strong> <Link to={"/device?sentinel=1&device_id=" + device.sentinel_id}>{device.sentinel_id}</Link></p> }
+                </div>
+            </div> : ""}
+            {sentinel === "1" ? "" : 
+                <Plot
+                    data={[
+                        {
+                        x: tempN.map(item => item.timestamp),
+                        y: tempN.map(item => item.value),
+                        type: 'scatter',
+                        mode: 'lines+markers',
+                        name: 'Temperature',
+                        },
+                    ]}
+                    layout={layout}
+                    style={{ width: '100%', height: '400px' }}
+                    />
+
+            }
+            { sentinel === "1" ? 
+                <section className="table-section">
+            <h2>Connected Relay Devices</h2>
+            <table>
+            <thead>
+                <tr>
+                <th>Device ID</th>
+                <th>Sentinel ID</th>
+                <th>Latitude</th>
+                <th>Longitude</th>
+                <th>Battery Life</th>
+                <th>Sentinel Connection</th>
+                <th>Is Connected</th>
+                <th>Last Online</th>
+                <th>Deployed</th>
+                <th>Deployed Date</th>
+                <th>Password</th>
+                </tr>
+            </thead>
+            <tbody>
+                {relayDevices.map((device, index) => (
+                <tr key={index} onClick={() => navigate(`/device?sentinel=0&device_id=${device.device_id}`)}>
+                    <td>{device.device_id}</td>
+                    <td>{device.sentinel_id}</td>
+                    <td>{device.latitude}</td>
+                    <td>{device.longitude}</td>
+                    <td>{device.battery_life}</td>
+                    <td>{device.sentinel_connection ? "Yes" : "No"}</td>
+                    <td>{device.is_connected ? "Yes" : "No"}</td>
+                    <td>{device.last_online}</td>
+                    <td>{device.deployed ? "Yes" : "No"}</td>
+                    <td>{device.deployed_date}</td>
+                    <td>{device.password}</td>
+                </tr>
+                ))}
+            </tbody>
+            </table>
+        </section> : ""}
+        </div>
+    </div>
+  );
+};
+
+export default DevicePage;
