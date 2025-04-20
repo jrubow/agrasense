dae726359839c4bfd68f87ed6598a2522c4483b4
Josh Rubow <143541334+jrubow@users.noreply.github.com>
2025-04-20 16:22:38 -0400

    Create power_monitor.ino

---
+++ devices/current/power_monitor.ino
@@ -0,0 +1,134 @@
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <gfxfont.h>
+
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ 0b00000000, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b11100000,
+  0b11110011, 0b11100000,
+  0b11111110, 0b11111000,
+  0b01111110, 0b11111111,
+  0b00110011, 0b10011111,
+  0b00011111, 0b11111100,
+  0b00001101, 0b01110000,
+  0b00011011, 0b10100000,
+  0b00111111, 0b11100000,
+  0b00111111, 0b11110000,
+  0b01111100, 0b11110000,
+  0b01110000, 0b01110000,
+  0b00000000, 0b00110000 };
+ 
+Adafruit_INA219 ina219;
+ 
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println("hello");
+  while (!Serial) 
+  {
+    delay(1);
+  }
+ 
+  Serial.println("hello1.4");
+  // Initialize the INA219.
+  if (! ina219.begin())
+  {
+    Serial.println("Failed to find INA219 chip");
+    while (1) 
+    {
+      Serial.println("hello2");
+      delay(10);
+    }
+  }
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.display();
+  delay(2000);
+  // To use a slightly lower 32V, 1A range (higher precision on amps):
+  //ina219.setCalibration_32V_1A();
+  // Or to use a lower 16V, 400mA range, call:
+  //ina219.setCalibration_16V_400mA();
+ 
+  Serial.println("Measuring voltage, current, and power with INA219 ...");
+}
+ 
+void loop(void)
+{
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
+ 
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+ 
+  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
+  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
+  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
+  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+  Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
+  Serial.println("");
+
+  printPower();
+ 
+  delay(1000);
+}
+
+void printPower(void) {
+  display.clearDisplay();
+
+  display.setTextSize(1.8); // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 0);
+
+  display.print("Load V:   ");
+  display.print(ina219.getBusVoltage_V() + (ina219.getShuntVoltage_mV() / 1000));
+  display.println(" mV");
+
+  display.print("Current:  ");
+  display.print(ina219.getCurrent_mA());
+  display.println(" mA");
+  
+  display.print("Power:   ");
+  display.print(ina219.getPower_mW());
+  display.println(" mW");
+
+  
+  display.display();      // Show initial text
+  delay(100);
+
+}
